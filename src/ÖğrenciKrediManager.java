184e9d8d389fddec0b78c51887b4e75e5f65703d
4dreamsoldier4 <ciragburak@gmail.com>
2021-08-04 11:13:03 +0300

    Initial commit

---
+++ src/ÖğrenciKrediManager.java
@@ -0,0 +1,8 @@
+public class ÖğrenciKrediManager extends BaseKrediManager{
+    public  double hesapla (double tutar){
+        return tutar * 1.10;
+
+    }
+}
+
+//overrideble=yani üzerine yazılabilir methodtur.
